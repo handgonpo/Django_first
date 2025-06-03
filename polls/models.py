a15e31a18a52cf08a27b521185dde3bd8e5f5065
Youjung <handgonpo@naver.com>
2025-06-03 09:11:27 +0900

    [feat] - Django 공식문서 Part3

--- polls/models.py
+++ polls/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
-
 
     def __str__(self):
         return self.question_text
@@ -11,7 +11,9 @@ class Question(models.Model):
     def was_published_recently(self):
         from django.utils import timezone
         import datetime
+
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -19,4 +21,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choice_text    
+        return self.choice_text
