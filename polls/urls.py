1960f6aa3ffdf9efb99e922bd1abbdf7560dd5e9
Youjung <handgonpo@naver.com>
2025-06-04 14:05:13 +0900

    [feat] - Django 공식문서 Part4

--- polls/urls.py
+++ polls/urls.py
@@ -4,9 +4,14 @@ from . import views
 app_name = "polls"
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    # 127.0.0.1:8000/polls/
-    path("<int:question_id>/", views.detail, name="detail"),
-    path("<int:question_id>/results/", views.results, name="results"),
+    # path("", views.index, name="index"),
+    # # 127.0.0.1:8000/polls/
+    # path("<int:question_id>/", views.detail, name="detail"),
+    # path("<int:question_id>/results/", views.results, name="results"),
+    # path("<int:question_id>/vote/", views.vote, name="vote"),
+    # CBV URLs
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
