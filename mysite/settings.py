af90af3d0fde6f832c38cf69de2d987488183db8
Youjung <handgonpo@naver.com>
2025-06-10 01:02:55 +0900

    [feat] -part7

--- mysite/settings.py
+++ mysite/settings.py
@@ -56,7 +56,8 @@ ROOT_URLCONF = "mysite.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "polls", "templates")],
+        # "DIRS": [os.path.join(BASE_DIR, "polls", "templates")],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -122,3 +123,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+# Question 모델 커스터마이징
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("제목", {"fields": ["question_text"], "classes": ["collapse"]}),
+        ("날짜정보", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
